e1ecabf87e53872c91b8377a920ffc7a8c586760
akash ghare <akash.ghare@webileapps.com>
2025-07-27 13:49:03 +0530

    portfolio app

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:akashstone22@gmail.com" target="_blank">
+            akashstone22@gmail.com
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href=" https://www.linkedin.com/in/akash-ghare-1a5470107/">
+            linkedin/akash-ghare
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Akashwebd?tab=repositories">
+            github.com/Akash
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
